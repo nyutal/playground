a6e16711cfa1918f8078f0d758a49b5e2a0f7a5a
Nadav Yutal <nyutal@oath.com>
2020-07-13 16:24:58 +0300

    https://testdriven.io/blog/combine-flask-vue/ implementation;

---
+++ flask_vue/method3_spa_with_ssr_nuxt/webapp/pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="container">
+    <p>{{ greeting }}</p>
+    <p>{{ flaskGreeting }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            greeting: 'Hello, Vue!',
+            flaskGreeting: ''
+        }
+    },
+    created: async function(){
+        const gResponse = await fetch("http://localhost:5000/greeting");
+        const gObject = await gResponse.json();
+        this.flaskGreeting = gObject.greeting;
+    }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
